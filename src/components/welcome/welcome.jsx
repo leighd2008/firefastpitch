fcd7ff6d0790bee7af5faaa2153aaf57b0146e6b
leighd2008 <leighd2008@gmail.com>
2019-08-12 11:09:47 -0400

    fixed formating issues

--- src/components/welcome/welcome.jsx
+++ src/components/welcome/welcome.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './welcome.scss'
+import "./welcome.scss";
 
 const Welcome = () => (
-  <div className='welcome'>
-    <div 
-      className='background-image'
+  <div className="welcome">
+    <div className="content">
+      <h1 className="title">
+        These TEAMS are on FIRE! Can you handle the heat?
+      </h1>
+    </div>
+    <div
+      className="background-image"
       style={{
-        backgroundImage: `url(${'https://images.unsplash.com/photo-1531206715517-5c0ba140b2b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'})`
+        backgroundImage: `url(${"https://images.unsplash.com/photo-1531206715517-5c0ba140b2b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"})`
       }}
     />
-    <div className='content'>
-      <h1 className='title'>These TEAMS are on FIRE! Can you handle the heat?</h1>
-    </div>
   </div>
-)
+);
 
 export default Welcome;
