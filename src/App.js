a6b8a2a6aa52559fdef4e34a20373b63140f113f
leighd2008 <leighd2008@gmail.com>
2019-08-26 08:32:03 -0400

    changed date on gear store

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage';
-import TeamPage from './pages/teamPage/teamPage';
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Tryouts from './components/tryouts/tryout'
+import HomePage from "./pages/homepage/homepage";
+import TeamPage from "./pages/teamPage/teamPage";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Tryouts from "./components/tryouts/tryout";
 // import FanWear from './components/fan_wear/fan_wear';
-
 
 function App() {
   return (
     <div>
       <Header />
-      <div className='fanWearBanner' style={{textAlign: 'center'}}>
-        <a href="https://www.bsnteamsports.com/shop/FF2019?utm_source=school&utm_medium=banner&utm_campaign=store_shop&utm_content=products" target="_blank" rel="noopener noreferrer"><h1 style={{color: 'red'}}>Click to Shop the 2019 FIRE FASTPITCH GEAR Store!</h1></a>
-        <h2>Hurry, shops closes 08/23/2019.</h2>
+      <div className="fanWearBanner" style={{ textAlign: "center" }}>
+        <a
+          href="https://www.bsnteamsports.com/shop/FF2019?utm_source=school&utm_medium=banner&utm_campaign=store_shop&utm_content=products"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1 style={{ color: "red" }}>
+            Click to Shop the 2019 FIRE FASTPITCH GEAR Store!
+          </h1>
+        </a>
+        <h2>Hurry, shops closes 08/28/2019.</h2>
       </div>
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/Teams' component={TeamPage} />
-        <Route path='/Tryouts' component={Tryouts} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/Teams" component={TeamPage} />
+        <Route path="/Tryouts" component={Tryouts} />
       </Switch>
       <Footer />
     </div>
--- src/components/all_teams/all_teams.jsx
+++ src/components/all_teams/all_teams.jsx
@@ -1,49 +1,54 @@
-import React from 'react';
+import React from "react";
 
-import Team from '../team/team';
-import './all_teams.scss'
-
+import Team from "../team/team";
+import "./all_teams.scss";
 
 class Teams extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      teams : [{
-        title: 'Fire 14U',
-        imageUrl: 'https://images.unsplash.com/photo-1507245991864-a005497bdab8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        id: 1,
-        linkUrl: 'Fire_14U'
-      },
-      {
-        title: 'Fire 12U',
-        imageUrl: 'https://images.unsplash.com/photo-1553270040-8e90b62a94f8?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-        id: 2,
-        linkUrl: 'Fire_12U'
-      },
-      {
-        title: 'Fire 10U',
-        imageUrl: 'https://images.unsplash.com/photo-1508802551395-fbecf2af43b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-        id: 3,
-        linkUrl: 'Fire_10U'
-      },
-      {
-        title: 'Fire 8U',
-        imageUrl: 'https://images.unsplash.com/photo-1535480436112-07697fcbcbea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        id: 4,
-        linkUrl: 'Fire_8U'
-      }]
-    }
+      teams: [
+        {
+          title: "Fire 14U",
+          imageUrl:
+            "https://images.unsplash.com/photo-1507245991864-a005497bdab8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+          id: 1,
+          linkUrl: "Teams"
+        },
+        {
+          title: "Fire 12U",
+          imageUrl:
+            "https://images.unsplash.com/photo-1553270040-8e90b62a94f8?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+          id: 2,
+          linkUrl: "Teams"
+        },
+        {
+          title: "Fire 10U",
+          imageUrl:
+            "https://images.unsplash.com/photo-1508802551395-fbecf2af43b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+          id: 3,
+          linkUrl: "Teams"
+        },
+        {
+          title: "Fire 8U",
+          imageUrl:
+            "https://images.unsplash.com/photo-1535480436112-07697fcbcbea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+          id: 4,
+          linkUrl: "Teams"
+        }
+      ]
+    };
   }
 
   render() {
-    return(
-      <div className='team-menu'>
-        {this.state.teams.map(({ id, ...otherTeamProps}) => (
-            <Team key={id} {...otherTeamProps} />
-          ))}
+    return (
+      <div className="team-menu">
+        {this.state.teams.map(({ id, ...otherTeamProps }) => (
+          <Team key={id} {...otherTeamProps} />
+        ))}
       </div>
-    )
+    );
   }
 }
 
