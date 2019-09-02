6137abf11a359abebeaa1cc40e7366c5b5b6f5a0
leighd2008 <leighd2008@gmail.com>
2019-09-02 11:20:11 -0400

    trying to get events in heroku

--- src/pages/teamPage/events.jsx
+++ src/pages/teamPage/events.jsx
@@ -1,4 +1,4 @@
-const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+// const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 
 let makeGoogleCalendarURL = calID =>
   `https://www.googleapis.com/calendar/v3/calendars/${calID}/events` +
