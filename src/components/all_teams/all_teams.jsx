7ae9efb7f913e7e25074e338e828dc261cc97800
leighd2008 <leighd2008@gmail.com>
2019-09-02 12:40:51 -0400

    added links to images on home page

--- src/components/all_teams/all_teams.jsx
+++ src/components/all_teams/all_teams.jsx
@@ -14,29 +14,29 @@ class Teams extends React.Component {
           imageUrl:
             "https://images.unsplash.com/photo-1507245991864-a005497bdab8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
           id: 1,
-          linkUrl: "Teams"
+          linkUrl: "Fire14U"
         },
         {
           title: "Fire 12U",
           imageUrl:
             "https://images.unsplash.com/photo-1553270040-8e90b62a94f8?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
           id: 2,
-          linkUrl: "Teams"
-        },
-        {
-          title: "Fire 10U",
-          imageUrl:
-            "https://images.unsplash.com/photo-1508802551395-fbecf2af43b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-          id: 3,
-          linkUrl: "Teams"
-        },
-        {
-          title: "Fire 8U",
-          imageUrl:
-            "https://images.unsplash.com/photo-1535480436112-07697fcbcbea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          id: 4,
-          linkUrl: "Teams"
+          linkUrl: "Fire12U"
         }
+        // {
+        //   title: "Fire 10U",
+        //   imageUrl:
+        //     "https://images.unsplash.com/photo-1508802551395-fbecf2af43b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        //   id: 3,
+        //   linkUrl: "Teams"
+        // },
+        // {
+        //   title: "Fire 8U",
+        //   imageUrl:
+        //     "https://images.unsplash.com/photo-1535480436112-07697fcbcbea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        //   id: 4,
+        //   linkUrl: "Teams"
+        // }
       ]
     };
   }
