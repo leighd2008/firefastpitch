9dc9cab35ccdea17043d9e454cef0e649bfe93ec
leighd2008 <leighd2008@gmail.com>
2020-06-27 18:43:23 -0400

    added tryout information

--- src/pages/tryoutspage/tryout.jsx
+++ src/pages/tryoutspage/tryout.jsx
@@ -10,17 +10,30 @@ const TryOuts = () => {
           <CardTitle className="tc f1" tag="h1">
             Are you ready to play with FIRE?
           </CardTitle>
-          <h2>
-            FIRE FASTPITCH Try-outs are over, however, we still have roster
-            spots to fill for 8U and 10U.{" "}
-          </h2>
-          <CardText tag="h1">
-            <h3>
-              If you or anyone you know is interested in discussing a coaching
-              opportunity or would like to get a private tryout for your
-              daughter please contact us.{" "}
-            </h3>
-          </CardText>
+          <h3>FIRE FASTPITCH will have open try outs for the teams in age 10U, 12U, 14U and 16U divisions on </h3> <br/> <h2>Saturday August 1st</h2> 
+          <br />
+          <h3>10U — 10:00 AM - 12:00 PM* ARIVE @ 9:30 AM</h3>
+          <h3>12U — 12:00 PM - 2:00 PM* ARIVE @ 11:30 AM</h3>
+          <h3>14U — 2:00 PM - 4:00 PM* ARIVE @ 1:30 PM</h3>
+          <h3>16U — 4:00 PM - 6:00 PM* ARIVE @ 3:30 PM</h3>
+          <br />
+          <h2>*PITCHERS & CATCHERS</h2> <h3> will need to possibly stay an additioinal 30 min to be evaluated separately</h3>
+          <br/>
+          <h2>LOCATION:</h2><h3>800 Gillmer Rd <br /> Leavittsbug Ohio 44430</h3>
+          <br />
+
+            <h2>REGISTRATION:</h2> <h3>Will be at Field #1. Info sheet and questionaire will need filled out before evaluations.</h3>
+          <br />
+          <h2>Wednesday Aug. 5th from 6-8 PM</h2>
+            <h3>We will follow that up with an all age group make up or call back on Wednesday Aug. 5th from 6-8 PM on all three fields simultaneously.</h3>
+          <br />
+
+            <h3>If you or anyone you know is interested in discussing a coaching opportunity or would like to get a private tryout for your daughter please contact us here by PM or eamil at: </h3>
+            <h2>Fire.Fastpitch.Softball@gmail.com</h2>
+                <br />
+            <h2>or call 330-980-1366</h2>
+                <br />
+            
         </CardBody>
       </Card>
     </div>
