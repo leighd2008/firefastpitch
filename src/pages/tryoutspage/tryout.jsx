d020162d0c4d2f74f0cde757f1f2495d234d16cf
leighd2008 <leighd2008@gmail.com>
2020-06-28 07:16:57 -0400

    fixed typos and alignment issues with tryouts page

--- src/pages/tryoutspage/tryout.jsx
+++ src/pages/tryoutspage/tryout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardTitle, CardText, CardBody } from "reactstrap";
+import { Card, CardTitle, CardBody } from "reactstrap";
 import "./tryout.scss";
 
 const TryOuts = () => {
@@ -10,30 +10,33 @@ const TryOuts = () => {
           <CardTitle className="tc f1" tag="h1">
             Are you ready to play with FIRE?
           </CardTitle>
-          <h3>FIRE FASTPITCH will have open try outs for the teams in age 10U, 12U, 14U and 16U divisions on </h3> <br/> <h2>Saturday August 1st</h2> 
+          <h3>FIRE FASTPITCH will have open tryouts for the teams in age 10U, 12U, 14U and 16U divisions on </h3> <br/> <h1>Saturday August 1st</h1> 
           <br />
           <h3>10U — 10:00 AM - 12:00 PM* ARIVE @ 9:30 AM</h3>
           <h3>12U — 12:00 PM - 2:00 PM* ARIVE @ 11:30 AM</h3>
           <h3>14U — 2:00 PM - 4:00 PM* ARIVE @ 1:30 PM</h3>
           <h3>16U — 4:00 PM - 6:00 PM* ARIVE @ 3:30 PM</h3>
           <br />
-          <h2>*PITCHERS & CATCHERS</h2> <h3> will need to possibly stay an additioinal 30 min to be evaluated separately</h3>
+          <h2>*PITCHERS & CATCHERS</h2> <h3> will need to stay an additional 30 min to be evaluated separately</h3>
           <br/>
-          <h2>LOCATION:</h2><h3>800 Gillmer Rd <br /> Leavittsbug Ohio 44430</h3>
+          <h2>LOCATION:</h2>
+          <a
+            href={`https://goo.gl/maps/TSZ2T6Ti4oo`}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <h3>800 Gillmer Rd <br /> Leavittsbug Ohio 44430</h3>
+          </a>
+          <h2>REGISTRATION:</h2> <h3>Will be at Field #1. Info sheet and questionaire will need filled out before evaluations.</h3>
           <br />
-
-            <h2>REGISTRATION:</h2> <h3>Will be at Field #1. Info sheet and questionaire will need filled out before evaluations.</h3>
+          <h1>Wednesday Aug. 5th from 6-8 PM</h1>
+          <h3>We will follow that up with an all age group make up or call back on Wednesday Aug. 5th from 6-8 PM on all three fields simultaneously.</h3>
           <br />
-          <h2>Wednesday Aug. 5th from 6-8 PM</h2>
-            <h3>We will follow that up with an all age group make up or call back on Wednesday Aug. 5th from 6-8 PM on all three fields simultaneously.</h3>
+          <h3>If you or anyone you know is interested in discussing a coaching opportunity or would like to get a private tryout for your daughter please contact us here by PM or email at: </h3>
+          <h2>Fire.Fastpitch.Softball@gmail.com</h2>
           <br />
-
-            <h3>If you or anyone you know is interested in discussing a coaching opportunity or would like to get a private tryout for your daughter please contact us here by PM or eamil at: </h3>
-            <h2>Fire.Fastpitch.Softball@gmail.com</h2>
-                <br />
-            <h2>or call 330-980-1366</h2>
-                <br />
-            
+          <h2>or call 330-980-1366</h2>
+          <br />
         </CardBody>
       </Card>
     </div>
