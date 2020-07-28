cd63d0a9ec8204bbf90539444d77c0f0d6018de2
leighd2008 <leighd2008@gmail.com>
2020-07-28 12:07:47 -0400

    got rid of arrows, removed last names, changed cacheing

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import ImageSlide from '../imageSlide/imageSlide';
-import Arrow from '../arrow/arrow'
 
 import './carousel.scss'
-
 
 const images = require.context("../../assets", true);
 
@@ -11,7 +9,7 @@ const imgUrls = [
   images(`./Chloe_1st_dinger.jpg`),
   images(`./Chloe_2cnd_dinger.jpg`),
   images(`./JenniferGriffin.jpg`),
-  images(`./MadusonZiegler.jpg`)
+  images(`./MadisonZiegler.jpg`)
 ];
 
 class Carousel extends Component {
@@ -21,9 +19,6 @@ class Carousel extends Component {
     this.state = {
       currentImageIndex: 0
     };
-
-    this.nextSlide = this.nextSlide.bind(this);
-    this.previousSlide = this.previousSlide.bind(this);
     this.setNextImage = this.setNextImage.bind(this);
   }
 
@@ -42,45 +37,11 @@ class Carousel extends Component {
     });
   }
 
-  previousSlide() {
-    const lastIndex = imgUrls.length - 1;
-    const { currentImageIndex } = this.state;
-    const shouldResetIndex = currentImageIndex === 0;
-    const index = shouldResetIndex ? lastIndex : currentImageIndex - 1;
-
-    this.setState({
-      currentImageIndex: index
-    });
-  }
-
-  nextSlide() {
-    const lastIndex = imgUrls.length - 1;
-    const { currentImageIndex } = this.state;
-    const shouldResetIndex = currentImageIndex === lastIndex;
-    const index = shouldResetIndex ? 0 : currentImageIndex + 1;
-
-    this.setState({
-      currentImageIndex: index
-    });
-  }
-
-
   render() {
 
     return (
       <div className="carousel">
-        <Arrow
-          direction='left'
-          clickFunction={this.previousSlide}
-          glyph='&#9664;' />
-        
         <ImageSlide url={imgUrls[this.state.currentImageIndex]} />
-        {/* <img src={imgsrc1} alt="first" /> */}
-        
-        <Arrow
-          direction="right"
-          clickFunction={this.nextSlide}
-          glyph='&#9654;' />
       </div>
     );
   }
