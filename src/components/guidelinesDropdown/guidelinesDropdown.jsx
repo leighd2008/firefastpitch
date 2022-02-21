aa499184760ae38d86100efb0c21168ff8dc535c
leighd2008 <leighd2008@gmail.com>
2022-02-21 13:27:00 -0500

    added USA and USSA rule books to guidelines drop down

--- src/components/guidelinesDropdown/guidelinesDropdown.jsx
+++ src/components/guidelinesDropdown/guidelinesDropdown.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import CoronaUpdate from "../../assets/Baseball-Softball.pdf"
 import OHSAArulesdoc from "../../assets/OHSAArulesdoc.pdf"
+import USArulesdoc from "../../assets/DigitalRulebook_Cover_USA.pdf"
+import USAArulesdoc from "../../assets/Fastpitch_Rules_USSA.pdf"
 
 import "./guidelinesDropdown.scss";
 
 const GuidelinesDropdown = () => (
   <div className="guidelines-dropdown">
     <div className="guidelines">
-      <a href={CoronaUpdate} target="_blank" rel="noopener noreferrer" ><h4>Corona Virus</h4>
+      <a href={USAArulesdoc} target="_blank" rel="noopener noreferrer" ><h4>USAA rules 2022</h4>
+      </a>
+      <a href={USArulesdoc} target="_blank" rel="noopener noreferrer" ><h4>USA rules 2022</h4>
       </a>
       <a href={OHSAArulesdoc} target="_blank" rel="noopener noreferrer" ><h4>OHSAA rules</h4>
+      </a>
+      <a href={CoronaUpdate} target="_blank" rel="noopener noreferrer" ><h4>Corona Virus</h4>
       </a>
     </div>
   </div>
