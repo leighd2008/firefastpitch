80444e161f4adbf3a32fe4cc3acbf6a16796a8d5
leighd2008 <leighd2008@gmail.com>
2022-06-15 21:27:06 -0400

    fixed arrival times for tryouts

--- src/pages/tryoutspage/tryout.jsx
+++ src/pages/tryoutspage/tryout.jsx
@@ -10,17 +10,17 @@ const TryOuts = () => {
           <CardTitle className="tc f1" tag="h1">
             Are you ready to play with FIRE?
           </CardTitle>
-          <h3>FIRE FASTPITCH team tryouts for all age groups will be </h3> 
+          <h3>FIRE FASTPITCH team tryouts for all age groups will be </h3>
           <h1>July 30st, July 31st, Aug. 6th & Aug. 7th</h1>
-          <h3>REGISTRATION:</h3> 
+          <h3>REGISTRATION:</h3>
           <a href="Preregistration" rel="noopener noreferrer">
             <h2>Pre-registration form can be filled out here!</h2>
           </a>
-           <h3> or at Field #1 on the day of tryouts. </h3>
+          <h3> or at Field #1 on the day of tryouts. </h3>
           <br />
-          <h2>10U(2012 & 2013) 12U(2010 & 2011) 14U(2008 & 2009) </h2><h3>10:00AM-12:00PM  - ARIVE @ 10:30AM</h3>
+          <h2>10U(2012 & 2013) 12U(2010 & 2011) 14U(2008 & 2009) </h2><h3>10:00AM-12:00PM  - ARIVE @ 9:30AM</h3>
           <br />
-          <h2>16U(2006 & 2007) 18U(2004 & 2005) </h2><h3>1:00-3:00PM  - ARIVE @ 1:00PM</h3>
+          <h2>16U(2006 & 2007) 18U(2004 & 2005) </h2><h3>1:00-3:00PM  - ARIVE @ 12:30PM</h3>
           <br />
           <h2>*PITCHERS & CATCHERS</h2> <h3> will need to stay an additional 30 min to be evaluated separately</h3>
           <br />
@@ -37,7 +37,7 @@ const TryOuts = () => {
             <h3>800 Gillmer Rd <br /> Leavittsbug Ohio 44430</h3>
           </a>
           <br />
-          
+
           <h2>If you or anyone you know is interested in discussing a coaching opportunity or would like to get a private tryout for your daughter please contact us: </h2>
           <h3>Fire.Fastpitch.Softball@gmail.com</h3>
           <br />
