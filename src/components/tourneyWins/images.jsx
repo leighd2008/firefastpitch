d915e507b921282351337f443e42557f58280950
Diane Leigh <leighd2008@gmail.com>
2022-06-19 20:55:04 -0400

    added 18U championship and homeruns

--- src/components/tourneyWins/images.jsx
+++ src/components/tourneyWins/images.jsx
@@ -1,5 +1,6 @@
 
 const imgUrls = [
+  `https://firebasestorage.googleapis.com/v0/b/fire-fastpitch.appspot.com/o/tourneyWins%2FSwingn_in_solon_18U_champions_2022.jpg?alt=media&token=d5fb416b-3117-490a-aae3-d4c32ccb909a`,
   `https://firebasestorage.googleapis.com/v0/b/fire-fastpitch.appspot.com/o/tourneyWins%2FShamrock_Classic_2022_10U_Champion.jpg?alt=media&token=e470de5e-7f06-4861-a30e-aa14c87cf3ed`,
   `https://firebasestorage.googleapis.com/v0/b/fire-fastpitch.appspot.com/o/tourneyWins%2FVeterans_Day_Classic_2021_2012_10U_2cd_place.jpg?alt=media&token=5a2f25c8-49f9-4127-a741-508e204da128`,
   `https://firebasestorage.googleapis.com/v0/b/fire-fastpitch.appspot.com/o/tourneyWins%2FPhantoms_Salute_To_Service_2021_2cnd_place.jpeg?alt=media&token=ccf0513e-d7c7-4642-8201-607e93a69af7`,
