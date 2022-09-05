3b8ba01bb7cde0cca372ce06c82145f787616beb
Diane Leigh <leighd2008@gmail.com>
2022-09-05 17:00:21 -0400

    added button for full registered database

--- src/pages/registeredpage/registeredpage.jsx
+++ src/pages/registeredpage/registeredpage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import Registered from "./registered";
+import CsvDownload from 'react-json-to-csv'
+import { selectRegisteredData } from "../../redux/registration/registration.selectors";
 
 import './registeredPage.scss'
 
-const RegisteredPage = () => {
+const RegisteredPage = (registeredData) => {
+    const registeredDataArray = Object.entries(registeredData);
   return (
     <div>
+    {console.log(registeredDataArray[0][1].Registered.players)}
       <h4 className="tc">This page displays all registered players, click here to view new registrations for 2022</h4>
       <Registered index={2} division={'10U'} />
       <br />
@@ -17,10 +23,16 @@ const RegisteredPage = () => {
       <br />
       <Registered index={0} division={'18U'}/>
       <br />
+      <h4>All registered players</h4>
+      <CsvDownload data={registeredDataArray[0][1].Registered.players} />
+      
       {/* <Registered index={2} /> */}
     </div>
   );
 }
+const mapStateToProps = createStructuredSelector({
+  registeredData: selectRegisteredData,
+})
 
-export default RegisteredPage;
+export default connect(mapStateToProps)(RegisteredPage);
 
