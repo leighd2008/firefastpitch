fb2ca71879146afb592a6c5ccfe81bbf14cfa94c
Diane Leigh <leighd2008@gmail.com>
2022-09-20 12:02:32 -0400

    switched to node 16.17.0, updated packages, fixed scrolling on team roster page table

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,7 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-import 'firebase/storage'
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import 'firebase/compat/storage'
 
 const config = {
   apiKey: `${process.env.REACT_APP_API_KEY}`,
