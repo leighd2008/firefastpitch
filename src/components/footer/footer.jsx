1a3b4aaf7e7acf9970e13a70e24b82aed5e5e5b2
Diane Leigh <leighd2008@gmail.com>
2022-10-27 16:12:09 -0400

    removed phone number from website

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="footer">
       <h1 className="title">Contact Information</h1>
       <h2>Email: Fire.Fastpitch.Softball@gmail.com</h2>
-      <h2>Phone: 330-980-1366</h2>
+      {/* <h2>Phone: 330-980-1366</h2> */}
       <p className="copyright">
         <a
           href="https://leighd2008.github.io/My_Profile/"
